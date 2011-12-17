25d5861c00250139061948c483af5a04171409cf
matt3ho <matt3ho@gmail.com>
2011-12-16 23:44:32 -0500

    v1

---
+++ Classes/Hello_WorldViewController.h
@@ -0,0 +1,16 @@
+//
+//  Hello_WorldViewController.h
+//  Hello World
+//
+//  Created by Snow Leopard User on 06/12/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Hello_WorldViewController : UIViewController {
+
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Snow Leopard User on 06/12/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
